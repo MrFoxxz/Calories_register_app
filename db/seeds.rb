76d086bef51054f3c25509cf742f5e2e027c07cf
MrFoxxz <migueljc1994@gmail.com>
2021-02-22 18:33:04 -0500

    Seeds!

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do
-    User.create(
-        email: Faker::Internet.email,
-        password: Faker::Number.number(digits: 3)
-    )
-end
+30.times do |user|
+    email = "usuario#{user+1}@mail.com"
+    password = '123456'
 
-10.times do
-    CaloriesRegister.create(
-        calories: Faker::Number.number(digits: 3),
-        description: Faker::Food.fruits,
-        calories_action: Faker::Boolean.boolean,
-        date: Faker::Date.between(from: 30.days.ago, to: Date.today),
-        user_id: "2"
+    User.create!(
+        email: email,
+        password: password, 
+        password_confirmation: password
     )
+
+    100.times do |regis|
+        if rand(1..2) == 1
+            calories_action = 'Perdí Calorías'
+        else
+            calories_action = 'Gane Calorías'
+        end
+        calories = rand(500..1000)
+
+        CaloriesRegister.create!(
+            calories_action: calories_action,
+            description: Faker::Food.fruits,
+            date: Faker::Date.between(from: 30.days.ago, to: Date.today),
+            user_id: User.last.id, 
+            calories: calories 
+        )
+    end
 end
